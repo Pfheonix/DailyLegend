f6a163efd57ed77a945ef1e347b1c963fa75304f
Pfheonix <d1stahl@gmail.com>
2018-01-11 10:04:49 -0500

    Added To Do List Entry layout and class. The current setup is highly problematic, will be changed today.

--- app/src/main/java/com/dylan_stahl/dailylegend/MainMenu.java
+++ app/src/main/java/com/dylan_stahl/dailylegend/MainMenu.java
@@ -1,7 +1,10 @@
 package com.dylan_stahl.dailylegend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        Button toDoSetupBtn = findViewById(R.id.ToDoSetupBtn);
+        Button gameSetupBtn = findViewById(R.id.GameSetupBtn);
+        Button characterBtn = findViewById(R.id.PlayerSetup);
+        Button settingsBtn = findViewById(R.id.SettingsBtn);
+    }
+
+    private void goToToDoList(View view){
+        Intent intent = new Intent(this, ToDoListEntry.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/dylan_stahl/dailylegend/ToDoListEntry.java
@@ -0,0 +1,17 @@
+package com.dylan_stahl.dailylegend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dylan on 1/10/18.
+ */
+
+public class ToDoListEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todolistentry);
+    }
+}
